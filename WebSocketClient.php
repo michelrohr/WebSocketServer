6755e8cb4bf1de80d7bdc4357f4b80d42fefb93e
Michel Rohr <courrier@michel.rohr.name>
2015-10-29 17:11:17 +0100

    Create WebSocketClient.php

---
+++ WebSocketClient.php
@@ -0,0 +1,13 @@
+class WebSocketClient {
+	// get according socket in WebSocketServer using $this->Sockets[$Client->ID]
+	public
+		$ID,
+		$Headers	= null,
+		$Handshake	= null,
+		$timeCreated= null;
+	
+	function __construct($Socket){
+		$this->ID			= intval($Socket);
+		$this->timeCreated	= time();
+	}
+}
